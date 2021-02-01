db87f9ece7f72e3590e2a809d30d2bdeecb6a74f
André Gaspar <and_lrg@hotmail.com>
2021-02-01 13:33:08 -0300

    Blog stricture and post skeleton

---
+++ static/src/js/main.js
@@ -0,0 +1,44 @@
+function build_from_github(name, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", "https://api.github.com/users/" + name, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    document.getElementsByName("from").forEach(function(el) {
+        let who = el.getAttribute("attr");
+        // let when = el.getAttribute("attr-when");
+
+        build_from_github(who, function(result) {
+            let json = JSON.parse(result);
+
+            let img = document.createElement('img');
+            img.className = "from-img";
+            img.src = json.avatar_url;
+            el.appendChild(img);
+
+            let span = document.createElement('span');
+            span.textContent = " by "
+            span.className = "by-text"
+            el.appendChild(span);
+
+            let a = document.createElement('a')
+            a.href = json.html_url;
+            a.innerText = json.name;
+            el.appendChild(a);
+
+            span = document.createElement('span');
+            span.textContent = " at "
+            span.className = "by-text"
+            el.appendChild(span);
+// date formatting
+        });
+    });
+
+});
